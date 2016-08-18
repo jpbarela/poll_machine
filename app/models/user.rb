429fe3324119242a5725e34a24f9435c8c8d3d27
J.P. Barela <barela@jpbarela.com>
2016-08-17 21:48:52 -0600

    Update user for poll feature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :polls
+
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :password, presence: true, confirmation: true, on: :create
