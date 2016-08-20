3269d163d767322379964a9d5fc8488a06cf92d9
J.P. Barela <barela@jpbarela.com>
2016-08-19 22:30:22 -0600

    Update session helpers.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,10 @@ module SessionsHelper
     session[:current_user]['user_id']
   end
 
+  def current_user_path
+    user_path current_user_id
+  end
+
   def log_in(user)
     session[:current_user] = {}
     session[:current_user]['user_id'] = user.id
